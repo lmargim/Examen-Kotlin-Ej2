97cec0d69713c3bd61cad0a6e7b9b2edf22dc112
Luis Martín <xlmargim360@ieshnosmachado.org>
2024-11-19 09:42:23 +0100

    Primer commit

---
+++ app/src/main/java/com/luismartingimeno/examenEJ2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.luismartingimeno.examenEJ2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.luismartingimeno.examenEJ2.listaColoresScreen.SelectorColores
+import com.luismartingimeno.examenEJ2.ui.theme.ExamenEJ2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ExamenEJ2Theme {
+                SelectorColores()
+
+            }
+        }
+    }
+}
