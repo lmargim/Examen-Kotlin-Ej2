5a038e666dfb8a2a1584977cf9c8c527cdef9be7
Luis Martín <xlmargim360@ieshnosmachado.org>
2024-11-19 10:53:43 +0100

    Tercer commit: Arreglo de ejercicio

--- app/src/main/java/com/luismartingimeno/examenEJ2/MainActivity.kt
+++ app/src/main/java/com/luismartingimeno/examenEJ2/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.luismartingimeno.examenEJ2.listaColoresScreen.ListaColores
+import com.luismartingimeno.examenEJ2.listaColoresScreen.ColorSeleccionApp
 import com.luismartingimeno.examenEJ2.ui.theme.ExamenEJ2Theme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExamenEJ2Theme {
-                ListaColores()
+                ColorSeleccionApp()
 
             }
         }
